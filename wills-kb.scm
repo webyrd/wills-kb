0b28c0841088e211f541379a9601f11b35d14aa0
Will Byrd <webyrd@gmail.com>
2017-03-05 01:02:41 -0700

    initial commit

---
+++ wills-kb.scm
@@ -0,0 +1,51 @@
+(load "faster-miniKanren/mk-vicare.scm")
+(load "faster-miniKanren/mk.scm")
+
+
+;; import pub med articles
+
+;; want to automatically grab abstract, keywords, authors, etc.  ridiculous to have to paste it in
+
+;; should be able to auto-import from web browser, email, text, etc
+
+;; sharable
+
+;; bidirectional links
+
+;; executable
+
+;; multimedia
+
+;; pub med, edx, MIT OCW, wikipedia
+
+;; super query over pubmed, Church lab, etc
+
+;; auto-notification of new articles on, for example, CRISPR + mitochrondria
+
+CRISPR/Cas9 and mitochondrial gene replacement therapy: promising techniques and ethical considerations
+https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5043096/
+
+
+Bacterial CRISPR/Cas DNA endonucleases: A revolutionary technology that could dramatically impact viral research and treatment.
+https://www.ncbi.nlm.nih.gov/pubmed/25759096
+
+
+Fermentation & Cellular Respiration
+https://ocw.mit.edu/high-school/biology/exam-prep/cellular-energetics/fermentation-cellular-respiration/
+
+
+HarvardX: MCB64.1x Cell Biology: Mitochondria
+https://courses.edx.org/courses/course-v1:HarvardX+MCB64.1x+2T2016/courseware/940c989b9d8a413bad3d511a5edcf593/45ca3dae63614e33957690e09dcde93f/
+
+
+Powerhouse Rules: The Role of Mitochondria in Human Diseases
+https://ocw.mit.edu/courses/biology/7-342-powerhouse-rules-the-role-of-mitochondria-in-human-diseases-spring-2011/
+
+
+Worm longevity studies
+https://dash.harvard.edu/handle/1/10579402
+https://dash.harvard.edu/bitstream/handle/1/11879687/3865717.pdf?sequence=1
+
+
+Fly longevity studies
+https://en.wikipedia.org/wiki/Michael_R._Rose
